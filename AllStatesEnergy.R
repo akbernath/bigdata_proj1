fe6fdee624e9e12c86eb0143ac571c1afca8656a
kitadah <heather.kitada@stat.oregonstate.edu>
2014-04-15 21:11:43 -0700

    Summarised data and write csv

--- AllStatesEnergy.R
+++ AllStatesEnergy.R
@@ -55,3 +55,35 @@ any(status == "failed to cut")
 
 # 4. read in a file, produce a summary, concatenate
 
+51*11 #elements of mat
+
+nrg_types<-matrix(rep(0,561),nrow=11)
+nrg_types[1,]<-c("State",state_names)
+nrg_types[,1]<-c("State","Utility Gas","Bottled, tank, or LP gas","Electricity","Fuel oil, kerosene, etc","Coal or coke","Wood","Solar engery","Other fuel","No fuel used",NA)
+nrg_types[,1]
+
+for(i in 1:50){
+  h=i+1
+  state<-read.csv(paste("/Users/heatherhisako1/ss12h",tolower(state.abb)[i],"-cut.csv",sep=""),header=TRUE)
+  state_df <- tbl_df(state)
+  state_df <- mutate(state_df, energy_type = HFL_codes[as.character(HFL)])
+  
+  state_nrg_type <- group_by(state_df, HFL)
+  
+  nrg_summary <- summarise(state_nrg_type, 
+                           energy_type = first(energy_type),
+                           n = n(),
+                           n_missing = sum(is.na(HFL)))
+  
+  nrg_summary2 <- mutate(nrg_summary, prop = n/sum(n))
+  for(j in 1:(length(nrg_summary2$energy_type)-1)){
+    for(k in 2:10){
+      if(nrg_summary2$energy_type[j]==nrg_types[k,1]){
+        nrg_types[k,h]=nrg_summary2$prop[j]
+      }
+    } 
+  }
+  nrg_types[11,h]=nrg_summary2$prop[length(nrg_summary2$energy_type)]
+}
+
+write.csv(nrg_types, file = "/Users/heatherhisako1/Documents/bigdata_proj1/nrg_types.csv")
