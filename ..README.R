615bb8bbe8e64e5567c8edae4dbaa8a7bd3b515c
ethan.edwards <edwardet@onid.oregonstate.edu>
2014-04-21 07:58:16 -0700

    Readme for Accessibility

---
+++ ..README.R
@@ -0,0 +1,14 @@
+# This is a readme file intended to make the Group 4 github more accessible.
+#
+# The github contains many files (all our plot pdfs, all our outdated code, 
+# and so on), and as such it may take time to sort through. This is intended 
+# as a reference index to allow for quick reading.
+#
+# FINAL PRESENTATION: Proj1Pres.pdf
+#   (Beamer code)   : Proj1Pres.tex
+#
+# Heather's R Code  : AllStatesEnergy.R
+# Ethan's R Code    : VeteranCareCode.R
+# Nandhita's R Code : Immigration.R
+#   (Assistance)    : CompareImmigrantIncomePlots.R
+# Andrew's R Code   : Commuting Code.R
